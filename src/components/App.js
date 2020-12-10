d464719c251121538f58e880be8c5924b2a1edc7
Hruday Bharadwaj <saihruday.08@gmail.com>
2020-12-11 01:19:44 +0530

    chngs

--- src/components/App.js
+++ src/components/App.js
@@ -2,36 +2,96 @@ import React, { Component, useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
+  let name = "";
+  let email = "";
+  let gender = "";
+  let phone = "";
+  let pass = "";
+
+  let [text, setText] = useState("");
+
+  let handleChange = (type, event) => {
+    if (type === "name") {
+      name = event.target.value;
+    } else if (type === "email") {
+      email = event.target.value;
+    } else if (type === "gender") {
+      gender = event.target.value;
+    } else if (type === "phone") {
+      phone = event.target.value;
+    } else if (type === "pass") {
+      pass = event.target.value;
+    } else {
+      return;
+    }
+  };
+
+  let handleClick = (event) => {
+    if (
+      name === "" ||
+      email === "" ||
+      gender === "" ||
+      phone === "" ||
+      pass === ""
+    ) {
+      setText("All fields are mandatory");
+    }
+  };
+
   return (
     <div id="main">
       <form>
-        <label for="name">Name:</label>
-        <input id="name" type="text" data-testid="name" />
+        <label htmlFor="name">Name: </label>
+        <input
+          id="name"
+          type="text"
+          data-testid="name"
+          onChange={(event) => handleChange("name", event)}
+        />
         <br />
         <br />
-        <label for="email">Email:</label>
-        <input id="email" type="email" data-testid="email" />
+        <label htmlFor="email">Email: </label>
+        <input
+          id="email"
+          type="email"
+          data-testid="email"
+          onChange={(event) => handleChange("email", event)}
+        />
         <br />
         <br />
-        <label for="gender">Gender:</label>
+        <label htmlFor="gender">Gender: </label>
         <input
           id="gender"
           type="text"
           data-testid="gender"
           defaultValue="male"
+          onChange={(event) => handleChange("gender", event)}
         />
         <br />
         <br />
-        <label for="phone">Phone:</label>
-        <input id="phone" type="number" data-testid="phoneNumber" />
+        <label htmlFor="phone">Phone: </label>
+        <input
+          id="phone"
+          type="number"
+          data-testid="phoneNumber"
+          onChange={(event) => handleChange("phone", event)}
+        />
         <br />
         <br />
-        <label for="pass">Phone:</label>
-        <input id="pass" type="password" data-testid="password" />
+        <label htmlFor="pass">Password: </label>
+        <input
+          id="pass"
+          type="password"
+          data-testid="password"
+          onChange={(event) => handleChange("pass", event)}
+        />
         <br />
         <br />
-        <button data-testid="submit">SUBMIT</button>
+        <button data-testid="submit" onClick={() => handleClick()}>
+          SUBMIT
+        </button>
       </form>
+      <p>{text}</p>
     </div>
   );
 };
