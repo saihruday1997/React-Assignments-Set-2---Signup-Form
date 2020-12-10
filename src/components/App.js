43f7ac9725f850ee969822e9955e0c76a81557dd
Hruday Bharadwaj <saihruday.08@gmail.com>
2020-12-11 00:07:52 +0530

    chngs

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <form>
+        <label for="name">Name:</label>
+        <input id="name" type="text" data-testid="name" />
+        <br />
+        <br />
+        <label for="email">Email:</label>
+        <input id="email" type="email" data-testid="email" />
+        <br />
+        <br />
+        <label for="gender">Gender:</label>
+        <input id="gender" type="text" data-testid="gender" />
+        <br />
+        <br />
+        <label for="phone">Phone:</label>
+        <input id="phone" type="number" data-testid="phoneNumber" />
+        <br />
+        <br />
+        <label for="pass">Phone:</label>
+        <input id="pass" type="password" data-testid="password" />
+        <br />
+        <br />
+        <button data-testid="submit">SUBMIT</button>
+      </form>
+    </div>
+  );
+};
 
 export default App;
